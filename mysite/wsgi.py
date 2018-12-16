4386fd69af133a00b18598ea0256faf9cd0ea794
stulu <stulenox79@gmail.com>
2018-12-16 10:48:10 -0300

    modified wsgi.py file.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,10 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
+#import os
+
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+#application = get_wsgi_application()
 import os
+import sys
+path = '/home/stulu/my-first-blog'
+# use your own PythonAnywhere username here
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
